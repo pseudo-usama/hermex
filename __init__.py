75336579367f15c678884ff027273c1218fb4197
Usama (PSEUDO) <pseudo.usama@gmail.com>
2025-07-16 12:52:08 +0500

    small fix

--- __init__.py
+++ __init__.py
@@ -1,7 +1,12 @@
 from .generate_imgs import (
     generate_imgs_with_initial_prompt,
+    generate_imgs_with_initial_prompt_and_n_prompts,
     generate_imgs_with_n_prompts
 )
 
 
-__all__ = ["generate_imgs_with_initial_prompt", "generate_imgs_with_n_prompts"]
+__all__ = [
+    "generate_imgs_with_initial_prompt",
+    "generate_imgs_with_initial_prompt_and_n_prompts",
+    "generate_imgs_with_n_prompts"
+]
